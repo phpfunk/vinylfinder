bc171070c1bfc13fc3096c09812388e6588b62bb
Jeff Johns <jeff.johns@formstack.com>
2016-01-31 23:06:32 -0500

    Updated curl options for security

--- lib/VinylFinder/Base.php
+++ lib/VinylFinder/Base.php
@@ -21,8 +21,8 @@ class Base {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
 
         $res          = curl_exec($ch);
         $this->info   = curl_getinfo($ch);
