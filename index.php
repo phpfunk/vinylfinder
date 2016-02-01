a1705eca0029b3df6e922a917070e97751d287f4
Jeff Johns <jeff.johns@formstack.com>
2016-01-31 21:26:40 -0500

    Fix namespace

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/lib/vendor/autoload.php';
 
 // Start the log
-VinylFinder\Base::printLog(date('Y-m-d H:i:s A'));
+\VinylFinder\Base::printLog(date('Y-m-d H:i:s A'));
 \VinylFinder\Base::printLog(' - Getting wantlist from discogs');
 
 // Get your discogs wantlist
