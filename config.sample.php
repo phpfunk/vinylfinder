aaf2fc8a4fcb0d93381db51d18871c8aac890596
Jeff Johns <jeff.johns@formstack.com>
2016-02-07 15:41:27 -0500

    Updated config sample

--- config.sample.php
+++ config.sample.php
@@ -4,5 +4,7 @@ define('DISCOGS_PERSONAL_TOKEN', '', true);
 define('DISCOGS_USERNAME', '', true);
 define('TO_ADDRESS', '', true);
 define('FROM_ADDRESS', '', true);
+define('EMAIL_SUBJECT', 'Discogs Wantlist on Ebay', true);
+define('MMG_SUBJECT', 'MelloMusicGroup Vinyl Updates', true);
 define('SENDGRID_KEY', '', true);
 define('DEBUG', true, true);
